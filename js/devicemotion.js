dbdd5a1243c79e9dec6d866eef45268e5e3d1fce
Nikolaus321 <39513910+Nikolaus321@users.noreply.github.com>
2018-07-03 23:07:16 +0200

    Add files via upload

--- js/devicemotion.js
+++ js/devicemotion.js
@@ -2,9 +2,9 @@ if (window.DeviceMotionEvent) {
     document.getElementById("dmeSupported").innerText = "OK - Device Motion wird unterstützt!";
     window.addEventListener('devicemotion', function(event) {
 
-        document.getElementById("xRotation").innerHTML = 'x ' + event.rotationRate.x.toFixed(2);
-        document.getElementById("yRotation").innerHTML = 'y ' + event.rotationRate.y.toFixed(2);
-        document.getElementById("zRotation").innerHTML = 'z ' + event.rotationRate.z.toFixed(2);
+        document.getElementById("xRotation").innerHTML = 'x ' + event.rotateX.toFixed(2);
+        document.getElementById("yRotation").innerHTML = 'y ' + event.rotateY.toFixed(2);
+        document.getElementById("zRotation").innerHTML = 'z ' + event.rotateZ.toFixed(2);
 
     });
 } else {
