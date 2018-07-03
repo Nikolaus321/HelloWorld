8d8708e5b9c496beb0376846de292f4df6b122fe
Nikolaus321 <39513910+Nikolaus321@users.noreply.github.com>
2018-07-03 09:45:18 +0200

    Add files via upload

---
+++ js/meineDatei.js
@@ -0,0 +1,6 @@
+document.getElementById("neueID").innerHTML = "test";
+document.getElementById("neueID").style.fontsize = "50px";
+document.getElementById("neueID").style.color = "red";
+document.write("Hallo erste Zeile");
+document.write("<br>");
+document.write("Hallo zweite Zeile");
---
+++ js/meineDatei2.js
@@ -0,0 +1,9 @@
+var checkbox = document.querySelector("input[id=checkbox1]");
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        alert('angekreuzt');
+    } else {
+        // Checkbox is not checked..
+        alert('nicht angekreuzt');
+    }
+});
